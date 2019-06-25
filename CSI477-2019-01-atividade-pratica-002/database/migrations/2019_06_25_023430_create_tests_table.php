f203f05a4359b7ad71f80376768c7d043a2dc97e
hcdias <hugo_root@yahoo.com.br>
2019-06-25 02:20:54 -0300

    Adicionar campos date, procedure_id e user_id a migration tests

--- CSI477-2019-01-atividade-pratica-002/database/migrations/2019_06_25_023430_create_tests_table.php
+++ CSI477-2019-01-atividade-pratica-002/database/migrations/2019_06_25_023430_create_tests_table.php
@@ -14,8 +14,19 @@ class CreateTestsTable extends Migration
     public function up()
     {
         Schema::create('tests', function (Blueprint $table) {
-            $table->bigIncrements('id');
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->integer('procedure_id')->unsigned();
+            $table->dateTime('date');
             $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+
+            $table->foreign('procedure_id')
+                ->references('id')
+                ->on('procedures');
         });
     }
 
