7ab1221ef383cf3accdeb33c17694e353f0f982e
hcdias <hugo_root@yahoo.com.br>
2019-06-25 13:01:34 -0300

    Criar relacionamento entre models

--- CSI477-2019-01-atividade-pratica-002/app/Procedure.php
+++ CSI477-2019-01-atividade-pratica-002/app/Procedure.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Procedure extends Model
 {
-    //
+    protected $fillable = [
+        'name','price','user_id'
+    ];
+
+    public function users()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function tests()
+    {
+        return $this->hasMany('App\Test');
+    }
 }
--- CSI477-2019-01-atividade-pratica-002/app/Test.php
+++ CSI477-2019-01-atividade-pratica-002/app/Test.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Test extends Model
 {
-    //
+    protected $fillable = [
+        'user_id','procedure_id','date'
+    ];
+
+    public function procedures()
+    {
+        return $this->belongsTo('App\Procedure');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
