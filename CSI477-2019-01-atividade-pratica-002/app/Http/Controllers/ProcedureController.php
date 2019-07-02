5306511e63c9d56d5ad4839edd0a862041d9f10b
hcdias <hugo_root@yahoo.com.br>
2019-07-01 23:16:54 -0300

    Adicionar ProcedureController

---
+++ CSI477-2019-01-atividade-pratica-002/app/Http/Controllers/ProcedureController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Procedure;
+use App\Http\Requests\ProcedureRequest;
+use Illuminate\Support\Facades\Auth;
+
+class ProcedureController extends Controller
+{
+    public function index(Procedure $model)
+    {
+        return view('procedures.index',['procedures'=>$model->all()]);
+    }
+
+    public function create()
+    {
+        return view('procedures.create');
+    }
+
+    public function store(ProcedureRequest $request, Procedure $model)
+    {
+        $model->create($request->merge(['user_id'=>Auth::user()->id])->all());
+
+        return redirect()->route('procedures.index')->withStatus(__('Procedure successfully created.'));
+    }
+
+    public function edit(Procedure $procedure)
+    {
+        return view('procedures.edit', compact('procedure'));
+    }
+
+    public function update(ProcedureRequest $request, Procedure $procedure)
+    {
+        $procedure->update($request->all());
+        return redirect()->route('procedures.index')->withStatus(__('Procedure successfully updated.'));
+    }
+
+    public function destroy(Procedure $procedure)
+    {
+        $procedure->delete();
+        return redirect()->route('procedures.index')->withStatus(__('Procedure successfully deleted.'));
+    }
+}
