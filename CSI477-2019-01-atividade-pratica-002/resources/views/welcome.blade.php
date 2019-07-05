4762f80c78a73265beadf63d61fc469df93d1c51
hcdias <hugo_root@yahoo.com.br>
2019-07-05 12:07:46 -0300

    Exibir procedimentos na pagina publica

--- CSI477-2019-01-atividade-pratica-002/resources/views/welcome.blade.php
+++ CSI477-2019-01-atividade-pratica-002/resources/views/welcome.blade.php
@@ -9,23 +9,25 @@
     </div>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="card card-stats">
-                    <div class="card-header card-header-success card-header-icon">
-                        <div class="card-icon">
-                            <i class="material-icons">store</i>
+            @foreach($procedures as $procedure)
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="card card-stats">
+                        <div class="card-header card-header-success card-header-icon">
+                            <div class="card-icon">
+                                <i class="material-icons">store</i>
+                            </div>
+                            <p class="card-category">R$ {{$procedure->price }}</p>
+                            <h3 class="card-title">{{ $procedure->name }}</h3>
                         </div>
-                        <p class="card-category">R$ 3,00</p>
-                        <h3 class="card-title">Vacina</h3>
-                    </div>
-                    <div class="card-footer">
-                        <div class="stats">
-                            <i class="material-icons "></i>
-                            <a href="#pablo">Quero contratar</a>
-                      </div>
+                        <div class="card-footer">
+                            <div class="stats">
+                                <i class="material-icons "></i>
+                                <a href="#pablo">Quero contratar</a>
+                          </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
