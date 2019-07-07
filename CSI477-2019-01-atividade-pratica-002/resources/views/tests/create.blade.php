41b694856e9d58d7abac5fd9fb4b8a8c7a7ff742
hcdias <hugo_root@yahoo.com.br>
2019-07-06 22:01:19 -0300

    Corrigir labels na criacao de tests

--- CSI477-2019-01-atividade-pratica-002/resources/views/tests/create.blade.php
+++ CSI477-2019-01-atividade-pratica-002/resources/views/tests/create.blade.php
@@ -5,7 +5,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
-          <form method="post" action="" autocomplete="off" class="form-horizontal">
+          <form method="post" action="{{ route('tests.store') }}" autocomplete="off" class="form-horizontal">
             @csrf
             @method('post')
 
@@ -17,11 +17,11 @@
               <div class="card-body ">
                 <div class="row">
                   <div class="col-md-12 text-right">
-                      <a href="{{ route('tests') }}" class="btn btn-sm btn-primary">{{ __('Back') }}</a>
+                      <a href="{{ route('tests.index') }}" class="btn btn-sm btn-primary">{{ __('Back') }}</a>
                   </div>
                 </div>
                 <div class="row">
-                  <label class="col-sm-2 col-form-label">{{ __('Name') }}</label>
+                  <label class="col-sm-2 col-form-label">{{ __('Procedure') }}</label>
                   <div class="col-sm-7">
                     <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
                       <input class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" id="input-name" type="text" placeholder="{{ __('Name') }}" value="{{ old('name') }}" required="true" aria-required="true"/>
