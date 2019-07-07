4d50ce0c1b49bab83bd85c529a1f6c944d1bf495
hcdias <hugo_root@yahoo.com.br>
2019-07-07 00:31:01 -0300

    Adicionar traducao para pt-BR

---
+++ CSI477-2019-01-atividade-pratica-002/resources/lang/pt-BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Senhas devem ter ao menos 8 caracteres e serem iguais na confirmação.',
+    'reset' => 'Sua senha foi resetada!',
+    'sent' => 'Foi enviado um email com o link para recadastramento da senha!',
+    'token' => 'O token de recadastramento da senha é inválido.',
+    'user' => "Não foi possível encontrar um usuário que possua o email indicado.",
+
+];
