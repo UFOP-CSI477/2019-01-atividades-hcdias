710b4a41943b0ae06875ee9035ffca11cf74876f
hcdias <hugo_root@yahoo.com.br>
2019-07-06 21:56:41 -0300

    Verificar tipo de usuario ao exibir dashboard

--- CSI477-2019-01-atividade-pratica-002/app/Http/Controllers/HomeController.php
+++ CSI477-2019-01-atividade-pratica-002/app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 use App\Procedure;
+use App\Test;
+use Illuminate\Support\Facades\Gate;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,13 @@ class HomeController extends Controller
     public function index()
     {
         $procedures = Procedure::all();
+        $tests = Test::all();
+        //usuario do tipo paciente
+        if(Gate::allows('manage-my-tests')){
+            return view('dashboard_pacient',['procedures'=>$procedures]);
+        }
+
+        //usuario do tipo admin ou operador
         return view('dashboard',['procedures'=>$procedures]);
     }
 
